97b23275af4954fc252b3aa82c558b4daedb98c2
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2018-04-23 15:04:51 +0900

    Implements of Key inputs

--- velocity.js
+++ velocity.js
@@ -21,7 +21,6 @@ const YOSUKE_FURUKAWA = "@yosuke_furukawa";
     app.renderer.resize(1024, 1024);
     app.stage.interactive = true;
     document.body.appendChild(app.view);
-    // scale to window size.
     scaleToWindow(app.renderer.view);
     window.addEventListener("resize", function(event){
         scaleToWindow(app.renderer.view);
@@ -36,23 +35,77 @@ function loadProgressHandler(loader, resource) {
     console.log(`progress: ${loader.progress}%, url: ${resource.url}, loading: ${resource.name}`);
 }
 let sprite;
-let state = play;
+let state;
 function setup(loader, res) {
+    state = play;
     sprite = new PIXI.Sprite(PIXI.loader.resources[YOSUKE_FURUKAWA].texture);
     sprite.position.set(100, 100);
     sprite.vx = 0;
     sprite.vy = 0;
+    sprite.accelerationX = 0;
+    sprite.accelerationY = 0;
+    sprite.frictionX = 1;
+    sprite.frictionY = 1;
+    sprite.speed = 0.2;
+    sprite.drag = 0.98;
     //sprite.anchor.set(0.5);
     sprite.scale.set(0.5, 0.5);
     app.stage.addChild(sprite);
+    let left = keyboard(37),
+        up = keyboard(38),
+        right = keyboard(39),
+        down = keyboard(40);
+    left.press = function () {
+        sprite.accelerationX = -sprite.speed;
+        sprite.frictionX = 1;
+    };
+    left.release = function () {
+        if (!right.isDown) {
+            sprite.accelerationX = 0;
+            sprite.frictionX = sprite.drag;
+        }
+    };
+    up.press = function () {
+        sprite.accelerationY = -sprite.speed;
+        sprite.frictionY = 1;
+    };
+    up.release = function () {
+        if (!down.isDown) {
+            sprite.accelerationY = 0;
+            sprite.frictionY = sprite.drag;
+        }
+    };
+    right.press = function () {
+        sprite.accelerationX = sprite.speed;
+        sprite.frictionX = 1;
+    };
+    right.release = function () {
+        if (!left.isDown) {
+            sprite.accelerationX = 0;
+            sprite.frictionX = sprite.drag;
+        }
+    };
+    down.press = function () {
+        sprite.accelerationY = sprite.speed;
+        sprite.frictionY = 1;
+    };
+    down.release = function () {
+        if (!up.isDown) {
+            sprite.accelerationY = 0;
+            sprite.frictionY = sprite.drag;
+        }
+    };
     app.ticker.add(delta => gameLoop(delta));
 }
 function gameLoop(delta) {
     state();
 }
 function play() {
-    sprite.vx = 1;
-    sprite.vy = 1;
+    sprite.vx += sprite.accelerationX;
+    sprite.vy += sprite.accelerationY;
+    sprite.vx *= sprite.frictionX;
+    sprite.vy *= sprite.frictionY;
+    // move
     sprite.x += sprite.vx;
     sprite.y += sprite.vy;
     let collision = contain(sprite, {
@@ -61,6 +114,7 @@ function play() {
         width: app.renderer.view.width,
         height: app.renderer.view.height,
     });
+    // check collision
     if (collision) {
         if (collision.has("left") || collision.has("right")) {
             sprite.vx = -sprite.vx;
@@ -90,4 +144,35 @@ function contain(sprite, container) {
     }
     if (collision.size === 0) collision = undefined;
     return collision;
+}
+function keyboard(keyCode) {
+    var key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    key.downHandler = function (event) {
+        if (event.keyCode === key.code) {
+            if (key.isUp && key.press) {
+                key.press();
+            }
+            key.isDown = true;
+            key.isUp = false;
+        }
+        event.preventDefault();
+    };
+    key.upHandler = function (event) {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) {
+                key.release();
+            }
+            key.isDown = false;
+            key.isUp = true;
+        }
+        event.preventDefault();
+    };
+    window.addEventListener("keydown", key.downHandler.bind(key), false);
+    window.addEventListener("keyup", key.upHandler.bind(key), false);
+    return key;
 }
